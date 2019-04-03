2dd5ce01a896b0f6ca954df0263ee4b9feada151
teamzz111 <felipeflogxd@gmail.com>
2019-04-02 23:40:30 -0500

    ReactNative navigation

--- Energias/App.js
+++ Energias/App.js
@@ -1,49 +1,38 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import  HomeScreen  from "./screens/HomeScreen";
+import CmpHeader from "./screens/staticComponent/Header";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+const AppNavigator = createStackNavigator(
+  {
+    Inicio: {
+      screen: HomeScreen,
+      navigationOptions:{
+        title: "Seleccione una opción",
+        header: <CmpHeader text = "BIENVENIDO, SELECCIONE UNA OPCIÓN" />
+      }
+    }
+
+  },
+  { // Se establece n los parametros para la pila de pantallas
+    initialRouteName: "Inicio", // Ruta inicial (pantalla inicio por defecto)
+    defaultNavigationOptions: {
+      headerStyle: {
+       
+      }
+    },
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>asdasd Erika!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+    return <AppContainer />;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
