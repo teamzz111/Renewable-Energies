841414c5e8ab4267f3cfa4b62e8fcf29bde39b6e
teamzz111 <felipeflogxd@gmail.com>
2019-04-19 02:43:01 -0500

    Terminado: TSV con latitud y longitud

--- Energias/screens/MenuTSM.js
+++ Energias/screens/MenuTSM.js
@@ -1,89 +1,96 @@
 import React, {} from 'react';
-import { Text, View, Picker, StyleSheet, TouchableNativeFeedback} from 'react-native';
+import { Text, View, Picker, StyleSheet, TouchableNativeFeedback, TextInput, Image,TouchableHighlight, ScrollView, Alert} from 'react-native';
 import DatePicker from 'react-native-datepicker'
 
+
 export default class HomeScreen extends React.Component  {
+  
+  
   constructor(props){
     super(props);
     this.state = {
       date: new Date(),
-      date2: "",
-      date3: "",
-      months:["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio","Agosto","Septiembre", "Octubre", "Noviembre", "Diciembre"] ,
+      diasSumados: [0, 31, 59,90,120,151,181,212,242,273,303,334],
+      date2: "2019-02-2",
+      date3: "22:00",
       languague: "",
-      options: false,
+      place:  false,
+      coord: false,
+      latlot: true,
       selected:  true,
-      renderOption: true
-
-      
+      renderOption: false,
+      x: "", 
+      y: "",
+      z: "",
+      latitud: "",
+      longitud: "",
+      TSV: ""
+    }
   }
-}
-
   renderOptions = () => {
     if(this.state.renderOption){
       return (          
-        <View style = {{flex: 1, marginTop: 10, alignItems: 'center'}}>
+        <View style = {{flex: 1, marginTop: 30, alignItems: 'center'}}>
         <Text style={styles.text}> ¿Cómo te gustaría ingresar este dato? </Text>
             <TouchableNativeFeedback
               onPress = {
-                () => this.setState({options: true, renderOption: false})
+                () => this.setState({place: true, renderOption: false})
               }
               background={TouchableNativeFeedback.SelectableBackground()}>
-              <View style={{ width: 220, height: 30, backgroundColor: 'red', marginTop: 24 }}>
+              <View style={styles.boton}>
                 <Text style={{ margin: 3, color: "white", textAlign: 'center'}}>Elegir lugar</Text>
               </View>
             </TouchableNativeFeedback>
             <TouchableNativeFeedback
-              onPress={this._onPressButton}
+              onPress = {
+                () => this.setState({
+                  coord: true,
+                  renderOption: false
+                })
+              }
               background={TouchableNativeFeedback.SelectableBackground()}>
-              <View style={{ width: 220, height: 30, backgroundColor: 'red', marginTop: 6 }}>
+              <View style={styles.boton}>
                 <Text style={{ margin: 3, color: "white", textAlign: 'center' }}>Ingresar coordenadas</Text>
               </View>
             </TouchableNativeFeedback>
             <TouchableNativeFeedback
-              onPress={this._onPressButton}
+              onPress = {
+                () => this.setState({
+                  latlot: true,
+                  renderOption: false
+                })
+              }
               background={TouchableNativeFeedback.SelectableBackground()}>
-              <View style={{ width: 220, height: 30, backgroundColor: 'red', marginTop: 6 }}>
+              <View style={styles.boton}>
                 <Text style={{ margin: 3, color: "white", textAlign: 'center' }}>Ingresar latitud y longitud</Text>
               </View>
             </TouchableNativeFeedback>
-
           </View>
+      );
+    }
+  }
 
-);
+  renderRefreshButton = () => {
+    if(!this.state.renderOption){
+      return (<View>
+        <TouchableHighlight style = {{width: 30}} onPress={() => this.reset()}>
+            <Image source={require('../resources/left-arrow.png')} />
+        </TouchableHighlight>
+      </View>);
     }
   }
-  renderPlace = () => {
-    if(this.state.options){
-      return(
-        <View style = {styles.datepicker}> 
-        <Text style={styles.text}> Bien.. Buena decisión, selecciona el lugar </Text>
-        <Picker
-        selectedValue={this.state.language}
-        style={{ height: 50, width: 200 }}
-        onValueChange={(itemValue, itemIndex) =>
-          this.setState({ language: itemValue })
-        }>
-        <Picker.Item label="Bogotá" value="java" />
-        <Picker.Item label="Santiago de Chile" value="js" />
-        </Picker>
-    </View>);
-    }
-  }
-  render() {
+
+  calendarComponent = () => {
     return (
-      <View style={styles.container}>
-        <Text style = {styles.text}> Selecciona la fecha </Text>
-        <View style = {styles.datepicker}>
-        
+      <View style = {styles.datepicker}>     
         <DatePicker
           style={{width: 200}}
           date={this.state.date2}
           mode="date" 
           placeholder="Selecciona una fecha"
           format="YYYY-MM-DD"
-          minDate="2019-05-01"
-          maxDate="2019-06-01"
+          minDate="2019-01-01"
+          maxDate="2019-12-31"
           confirmBtnText="Confirm"
           cancelBtnText="Cancel"
           customStyles={{
@@ -103,7 +110,6 @@ export default class HomeScreen extends React.Component  {
         <DatePicker
           style={{width: 200, marginTop: 10}}
           date={this.state.date3}
-
           mode = "time"
           placeholder="Selecciona la hora"
           format = "HH:mm"
@@ -125,13 +131,238 @@ export default class HomeScreen extends React.Component  {
           }}
           onDateChange={(date) => {this.setState({date3: date})}}
         />
+      </View>);
+  }
+
+  reset = () => {
+    this.setState({latlot: false, renderOption: true, coord: false, place: false})
+  }
+
+  renderPlace = () => {
+    if(this.state.place){
+      return(
+        <View style = {styles.datepicker}> 
+          <Text style={styles.text}> Bien.. Buena decisión, selecciona el lugar </Text>
+          <Picker
+            selectedValue={this.state.language}
+            style={{ height: 50, width: 200 }}
+            onValueChange={(itemValue, itemIndex) =>
+              this.setState({ language: itemValue })
+            }>
+            <Picker.Item label="Bogotá" value="java" />
+            <Picker.Item label="Santiago de Chile" value="js" />
+          </Picker>
+        </View>);
+    } else if(this.state.coord){
+
+      return(
+        <View style = {styles.datepicker}> 
+          <Text style={styles.text}> Bien.. Buena decisión, ingresa los datos de las coordenadas y latitud, respectivamente </Text>
+          <View style = {{flex: 1, flexDirection: "row", marginTop: 20}}>
+            <TextInput
+              style={styles.inputCoord}
+              onChangeText={(text) => this.setState({x: text})}
+              value={this.state.x}
+              keyboardType = 'numeric'
+              />
+              <Text style = {styles.labelTextInputCoord}>°</Text>
+              <TextInput
+              style={styles.inputCoord}
+              onChangeText={(text) => this.setState({y: text})}
+              value={this.state.y}
+              keyboardType = 'numeric'
+              />
+              <Text style = {styles.labelTextInputCoord}> '</Text>
+              <TextInput
+              keyboardType = 'numeric'
+              style={styles.inputCoord}
+              onChangeText={(text) => this.setState({z: text})}
+              value={this.state.z}
+              />
+              <Text style = {styles.labelTextInputCoord}>''</Text>
+            
+          </View>
+          <View style = {{marginTop: 6}}>
+            <TextInput
+              keyboardType = 'numeric'
+              style={styles.inputCoord}
+              onChangeText={(text) => this.setState({z: text})}
+              value={this.state.z}
+              />
+          </View>
+          
+          <TouchableNativeFeedback
+              onPress = {
+                () => this.setState({
+                  latlot: true,
+                  renderOption: false
+                })
+              }
+              background={TouchableNativeFeedback.SelectableBackground()}>
+              <View style={styles.calc}>
+                <Text style={{ margin: 3, color: "white", textAlign: 'center' }}>Calcular</Text>
+              </View>
+            </TouchableNativeFeedback>
+    </View>);
+    } else if(this.state.latlot){
+      return(
+        <View style = {styles.datepicker}> 
+          <Text style={styles.text}> Bien.. Buena decisión, ingresa latitud y longitud </Text>
+          <View style = {{flex: 1, flexDirection: "column", marginTop: 20}}>
+            <TextInput
+              style={styles.inputLat}
+              onChangeText={(text) => this.setState({latitud: text})}
+              value={this.state.latitud}
+              keyboardType = 'numeric'
+              placeholder = "Latitud"
+              />
+            
+              <TextInput
+              style={styles.inputLat}
+              onChangeText={(text) => this.setState({longitud: text})}
+              value={this.state.longitud}
+              keyboardType = 'numeric'
+              placeholder = "Longitud"
+            
+              />
+          </View>
+          
+          <TouchableNativeFeedback
+              onPress = {
+                () => this.TSV(0)
+              }
+              background={TouchableNativeFeedback.SelectableBackground()}>
+              <View style={styles.calc}>
+                <Text style={{ margin: 3, color: "white", textAlign: 'center' }}>Calcular</Text>
+              </View>
+            </TouchableNativeFeedback>
+    </View>);
+    }
+  }
+
+  renderResultado = () => {
+    if(this.state.TSV != ""){
+
+      return (
+        <View style = {{flex: 1, alignItems: 'center', marginTop: 20}}>
+          <View style = {styles.resultados}>
+            <Text style = {styles.text}>
+              Resultados
+            </Text>
+            <Text style = {styles.textResultado}>
+              TSV:
+              {this.state.TSV}
+            </Text>
+        </View>
       </View>
-      {this.renderOptions()}
-      {this.renderPlace()}
-          <View style = {styles.datepicker}>
-              <Text style = {styles.text}>Ok. Ingresa los datos</Text>
-           </View>
-      </View>
+    );
+  }
+  }
+  /* Funciones lógicas */
+
+  diaAno = (day, month) => {
+    return Number(this.state.diasSumados[month-1]) + Number(day);
+  }
+
+  calculo_D = (dia) => {
+      return ((dia - 81) * 360) / 365;
+  }
+
+  minAHora = (TSV) =>{
+    let R, V, S;
+    let t;
+    R= parseInt(TSV / 60);
+    V=60*R;
+    S=parseInt(TSV - V);
+
+    if (S<10)
+        t=R+":0"+S;
+    else
+        t=R+":"+S;//Son las R:S
+    return t;
+  }
+
+  horaAMin = (hora, minuto) =>{
+    return hora * 60 + minuto;
+  } 
+
+  correccionL = (lat, lon) => {
+    return CL = 4 * (lat - lon);
+  }
+
+  toRadians = (angle) =>{
+    return (Math.PI * angle) / 180;
+  }
+
+  EcuacionTiempo = (D) =>{
+    return (9.87 * Math.sin(this.toRadians(2 * D))) - (7.57 * Math.cos(this.toRadians(D))) - (1.5 * Math.sin(this.toRadians(D)));
+  }
+
+  calcTSV = (CL, Et, Hora) =>{
+    return Hora + CL + Et;
+  }
+
+  TSV = (option) =>{
+    if(this.state.date2 == "" || this.state.date3 == ""){
+        Alert.alert(
+          'Atención',
+          'Campos de fecha y hora están vacíos',
+          [
+            {
+              text: 'Entendido',
+              onPress: () => console.log('OK Pressed')
+            },
+          ], {
+            cancelable: false
+          },
+        );
+    } else {
+
+      switch (option) {
+        case 0:
+          if(this.state.latitud == "" || this.state.longitud == ""){
+            Alert.alert(
+              'Atención',
+              'Campos de latitud y longitud están vacíos',
+              [{
+                text: 'Entendido',
+                onPress: () => console.log('OK Pressed')
+              }, ], {
+                cancelable: false
+              },
+            );
+          }
+          let splited = this.state.date2.split("-");
+          let dia = this.diaAno(splited[2], splited[1]);
+          let latitud = Number(this.state.latitud);
+          let longitud = Number(this.state.longitud);
+          splited = this.state.date3.split(":");
+          let hora = Number(splited[0]);
+          let minutos = Number(splited[1]);
+          this.setState({TSV: this.minAHora(this.calcTSV(this.correccionL(latitud, longitud), this.EcuacionTiempo(this.calculo_D(dia)), this.horaAMin(hora, minutos)))})
+
+        break;
+        
+        default:
+        break;
+      }
+    } 
+  }
+
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+
+        {this.calendarComponent()}
+        {this.renderRefreshButton()}
+        {this.renderOptions()}
+        {this.renderPlace()}
+        {this.renderResultado()}
+        <View style = {styles.datepicker}>
+          <Text style = {styles.text}></Text>
+        </View>
+
+      </ScrollView>
     );
   }
 }
@@ -151,6 +382,40 @@ const styles = StyleSheet.create({
   text:{
     fontSize: 16,
     textAlign: 'center',
-    marginTop: 20
+    marginTop: 20,
+    color: "black"
+  },
+  textResultado:{
+    fontSize: 16,
+    textAlign: 'left',
+    marginTop: 20,
+    color: "black"
+  },
+  boton:{
+    width: 220, height: 46, backgroundColor: 'red', marginTop: 12, padding: 10
+  },
+  inputCoord:{
+    height: 42, width: 50, borderColor: 'black', borderWidth: 0, borderBottomWidth: 1, fontSize: 22, textAlign: 'center', textAlignVertical: 'bottom'
+  },
+  labelTextInputCoord:{
+    fontSize: 30, marginTop: -2, color: 'black'
+  },
+  calc:{
+    width:'60%',
+    backgroundColor: 'red',
+    padding: 11,
+    marginTop: 29
+  },
+  inputLat:{
+    borderBottomWidth: 1,
+    width: 200
+  },
+
+  resultados:{
+    width: 220,
+    borderWidth: 1,
+    padding: 10,
+    paddingBottom: 20,
+    borderColor: "rgba(0,0,0,0.5)"
   }
 });
